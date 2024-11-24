c835c5c0151e55b69b4a24013a81a1bd8188442d
batreller <batreller@gmail.com>
2024-11-24 05:17:56 +0000

    increase jwt token expiry time

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ DATABASE_URL = "postgresql+asyncpg://username:password@localhost/vacation-vibes"
 # JWT configuration
 SECRET_KEY = "supersecretkey"
 JWT_ALGORITHM = "HS256"
-JWT_EXPIRY_TIME = 180  # expiry time in minutes
+JWT_EXPIRY_TIME = 5_256_000  # 10 years (expiry time in minutes)
 
 # logging configuration
 LOGGING_LEVEL = logging.DEBUG
